348f0997d6807e25a800b625db98c28e8a099036
tkmct <1220t.takamichi@gmail.com>
2022-09-07 20:36:51 +0900

    Mobile page layout

--- src/pages/mobile.tsx
+++ src/pages/mobile.tsx
@@ -1,5 +1,9 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
+import { SingleContainer } from '../components/Layout'
+import Logo from '../components/Logo'
+import { PageTitle, Description } from '../components/Text'
 import ROUTES from '../routes'
 import { isMobile } from '../utils'
 
@@ -16,7 +20,34 @@ const MobilePage = () => {
     // eslint-disable-next-line
   }, [])
 
-  return <div>Mobile is not supported</div>
+  return (
+    <Container>
+      <Logo inverse />
+
+      <Section>
+        <Title>KZG Ceremony</Title>
+        <Description>
+          This experience only avaliable on the desktop setting. See you there.
+        </Description>
+      </Section>
+    </Container>
+  )
 }
+
+const Container = styled(SingleContainer)`
+  padding: 12px 24px;
+`
+
+const Section = styled.div`
+  position: absolute;
+  bottom: 40px;
+`
+
+const Title = styled(PageTitle)`
+  color: ${({ theme }) => theme.onPrimary};
+  margin-top: 0;
+  font-size: 54px;
+  line-height: 54px;
+`
 
 export default MobilePage
