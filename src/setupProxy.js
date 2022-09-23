a806d227b7d19e2d131bf93138f282080cd04b71
Nico Serrano <nnicosp@hotmail.com>
2022-09-23 11:37:24 -0500

    wasm(fix): use chrome token to deactivate crossOriginIsolated in prod

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Cross-Origin-Embedder-Policy", "require-corp");
-    res.header("Cross-Origin-Opener-Policy", "same-origin");
-    next();
-  });
-};
