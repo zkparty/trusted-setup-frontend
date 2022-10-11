be649559ec61c7fa88c09a71c67b7ec73ad0c7f7
Nico Serrano <nnicosp@hotmail.com>
2022-10-10 20:56:39 -0500

    design(feat): add Testnet banner

--- src/components/HeaderJustGoingBack.tsx
+++ src/components/HeaderJustGoingBack.tsx
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
 import ROUTES from '../routes'
 import LeftArrow from '../assets/left-arrow.svg'
+import { FONT_SIZE } from '../constants'
+import { textSerif } from '../style/utils'
 
 const HeaderJustGoingBack = () => {
   const navigate = useNavigate()
@@ -10,7 +12,11 @@ const HeaderJustGoingBack = () => {
     <Container>
       <LeftSection onClick={() => navigate(ROUTES.ROOT)}>
         <img src={LeftArrow} alt="go back" />
+
       </LeftSection>
+      <CenterSection>
+        TESTNET CEREMONY
+      </CenterSection>
     </Container>
   )
 }
@@ -21,7 +27,7 @@ const Container = styled.header`
   top: 0;
   width: 100vw;
   background-color: ${({ theme }) => theme.surface2};
-  height: 80px;
+  height: 40px;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -33,6 +39,16 @@ const LeftSection = styled.div`
   justify-content: space-between;
   align-items: center;
   cursor: pointer;
+`
+
+const CenterSection = styled.div`
+  display: flex;
+  color: #3E70BC;
+  align-items: start;
+  font-size: ${FONT_SIZE.XXL};
+  ${textSerif}
+  font-weight: 500;
+  letter-spacing: 2px;
 `
 
 export default HeaderJustGoingBack
