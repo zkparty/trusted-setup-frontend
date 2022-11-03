0dce42c5c1800f7cc16c6f59cf6f3eea4b53c962
Nico Serrano <nnicosp@hotmail.com>
2022-11-03 14:47:54 -0500

    lobbyFull(fix): add header and background

--- src/pages/lobbyFull.tsx
+++ src/pages/lobbyFull.tsx
@@ -3,9 +3,11 @@ import { useNavigate } from 'react-router-dom'
 import { Trans, useTranslation } from 'react-i18next'
 import { PrimaryButtonLarge } from '../components/Button'
 import { Description, PageTitle } from '../components/Text'
+import HeaderJustGoingBack from '../components/HeaderJustGoingBack'
 import {
   SingleContainer as Container,
   SingleWrap as Wrap,
+  OverRelative,
   TextSection,
   InnerWrap,
   Over,
@@ -17,35 +19,40 @@ const LobbyFullPage = () => {
   const navigate = useNavigate()
 
   return (
-    <Over>
-      <Container>
-        <Wrap>
-        <InnerWrap>
-          <PageTitle>
-            <Trans i18nKey="lobbyFull.title">Too much <br /> magic</Trans>
-          </PageTitle>
-          <TextSection>
-            <Trans i18nKey="lobbyFull.description">
-              <Description>
-              Too many summoners at this time, please come back later.
-              Close this window and try again in a moment to check
-              if there is a slot available in the lobby.
-              </Description>
-            </Trans>
-          </TextSection>
-          <ButtonSection>
-            <PrimaryButtonLarge
-              onClick={() => {
-                navigate(ROUTES.ROOT)
-              }}
-            >
-              <Trans i18nKey="lobbyFull.button">Return to home</Trans>
-            </PrimaryButtonLarge>
-          </ButtonSection>
-        </InnerWrap>
-        </Wrap>
-      </Container>
-    </Over>
+    <>
+      <HeaderJustGoingBack />
+      <OverRelative>
+        <Over>
+          <Container>
+            <Wrap>
+            <InnerWrap>
+              <PageTitle>
+                <Trans i18nKey="lobbyFull.title">Too much <br /> magic</Trans>
+              </PageTitle>
+              <TextSection>
+                <Trans i18nKey="lobbyFull.description">
+                  <Description>
+                  Too many summoners at this time, please come back later.
+                  Close this window and try again in a moment to check
+                  if there is a slot available in the lobby.
+                  </Description>
+                </Trans>
+              </TextSection>
+              <ButtonSection>
+                <PrimaryButtonLarge
+                  onClick={() => {
+                    navigate(ROUTES.ROOT)
+                  }}
+                >
+                  <Trans i18nKey="lobbyFull.button">Return to home</Trans>
+                </PrimaryButtonLarge>
+              </ButtonSection>
+            </InnerWrap>
+            </Wrap>
+          </Container>
+        </Over>
+      </OverRelative>
+    </>
   )
 }
 
