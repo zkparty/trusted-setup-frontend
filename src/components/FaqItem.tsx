a33a9acabf73cddd080f8054f968ae7ac5ae14f1
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-11-07 19:20:19 -0800

    Update FaqItem component
    enable styling of JSX children of Content, and limits the open/close toggle click-zone to only the title text, preventing overlap of this click-zone with the links within

--- src/components/FaqItem.tsx
+++ src/components/FaqItem.tsx
@@ -5,17 +5,16 @@ import { textSerif } from '../style/utils'
 
 type Props = {
   title: string
-  content: string
+  content: string | React.ReactNode | JSX.Element
 }
 
 const FaqItem = ({ title, content }: Props) => {
   const [open, setOpen] = useState(false)
 
   return (
-    <Container onClick={() => setOpen(!open)}>
-      <Title>
-        <span>{title}</span>
-        <span>{open ? '-' : '+'}</span>
+    <Container>
+      <Title onClick={() => setOpen((prev) => !prev)} open={open}>
+        {title}
       </Title>
       {open && <Content>{content}</Content>}
     </Container>
@@ -24,22 +23,42 @@ const FaqItem = ({ title, content }: Props) => {
 
 const Container = styled.div`
   border-bottom: solid 1px ${({ theme }) => theme.primary};
-  cursor: pointer;
   transition: background-color 0.1s linear;
-
+  padding: 0 1rem; 
   :hover {
     background-color: #d6d6d6;
   }
 `
 
-const Title = styled.p`
+const Title = styled.p<{ open: boolean }>`
   font-size: ${FONT_SIZE.XXL};
   font-weight: 600;
   ${textSerif}
   display: flex;
   justify-content: space-between;
+  gap: 1rem;
+  padding-block: 2rem;
+  margin-block: 0;
+  :hover {
+      cursor: pointer;
+  }
+  ::after {
+    content: "${({ open }) => open ? '-' : '+'}";
+  }
 `
 
-const Content = styled.p``
+const Content = styled.div`
+  ol, li {
+    margin-block-end: 0.5rem;
+  }
+  blockquote {
+    border-inline-start: 2px solid ${({ theme }) => theme.disabled};
+    margin-inline: 1.5rem;
+    padding-inline: 1rem;
+  }
+  a {
+    text-decoration: underline;
+  }
+`
 
 export default FaqItem
