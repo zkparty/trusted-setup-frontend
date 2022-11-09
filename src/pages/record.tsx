4c171de5ed72448bf0f883dc516143e850f02e26
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-11-08 19:41:50 -0800

    Update record page
    updates layout, enables search/filtering, implements pagination

--- src/pages/record.tsx
+++ src/pages/record.tsx
@@ -1,17 +1,59 @@
+// Library imports
+import { useState, useMemo } from 'react'
 import styled from 'styled-components'
+// Component imports
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-import { SingleContainer as Container } from '../components/Layout'
+import Pagination from '../components/Pagination'
 import RecordTable from '../components/RecordTable'
-import { FONT_SIZE } from '../constants'
-import useRecord from '../hooks/useRecord'
-import SearchIcon from '../assets/search.svg'
 import { PageTitle } from '../components/Text'
+// Constant imports
+import { FONT_SIZE } from '../constants'
+// Asset imports
+import SearchIcon from '../assets/search.svg'
+// Hook imports
+import useRecord, { Record } from '../hooks/useRecord'
 
+// Constants
+const PAGE_SIZE = 20
+
+// RecordPage component
 const RecordPage = () => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [page, setPage] = useState(1)
+
   // add search param
   const recordQuery = useRecord()
 
+  // Helper function
+  const recordString = ({ id, sequenceNumber, publicKey, transcript }: Record): string => 
+    (id + sequenceNumber.toString() + publicKey + transcript).toLowerCase()
+
+  // Memoized data
+  const data = useMemo<Record[]>(() => {
+    if (!recordQuery.data) return []
+    // Sort data by sequence number, descending
+    const sortedData = recordQuery.data.sort((a, b) => b.sequenceNumber - a.sequenceNumber)
+    // Filter by search query
+    const filteredData = sortedData.filter((record) => recordString(record).includes(searchQuery.toLowerCase()))
+    return filteredData
+  }, [recordQuery, searchQuery])
+
+  const pageData = useMemo<Record[]>(() => {
+    // Slice by page
+    const sliceStart = (page - 1) * PAGE_SIZE
+    const sliceEnd = sliceStart + PAGE_SIZE > data.length ? data.length : sliceStart + PAGE_SIZE
+    const slicedData = data.slice(sliceStart, sliceEnd)
+    return slicedData
+  }, [data, page])
+
+  const totalPages = useMemo<number>(() => data ? Math.ceil(data.length / PAGE_SIZE) : 0, [data])
+
+  // Handler functions
+  const handleInput = (e: any) => {
+    setSearchQuery(e.target.value)
+  }
+  
   return (
     <>
       <Header />
@@ -19,16 +61,28 @@ const RecordPage = () => {
         <PageTitle>
           Record
         </PageTitle>
-        <SearchInput placeholder="Search address, github handle..." />
+        <SearchInput placeholder="Search address, github handle..." onChange={handleInput} />
         <RecordTable
-          data={recordQuery.data || []}
+          data={pageData}
           isLoading={recordQuery.isLoading}
         />
+        <Pagination page={page} setPage={setPage} totalPages={totalPages} />
       </Container>
       <Footer />
     </>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ${({ theme }) => theme.text};
+  width: 90ch;
+  max-width: 100%;
+  margin: 8rem auto;
+  padding-inline: 5vw;
+`
 
 const SearchInput = styled.input`
   font-size: ${FONT_SIZE.M};
