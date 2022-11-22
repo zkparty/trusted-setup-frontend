50098d3490be1adc84e487754aa3827c1b6a29d8
Nico Serrano <nnicosp@hotmail.com>
2022-11-21 22:41:29 -0500

    sequencer(fix): better error handling in network problems

--- src/hooks/useSequencerStatus.ts
+++ src/hooks/useSequencerStatus.ts
@@ -4,27 +4,19 @@ import { SequencerStatus } from '../types'
 
 export default function useSequencerStatus() {
   return useQuery(['status'], async () => {
-    return new Promise<SequencerStatus>((resolve) => {
-      fetch(API_ROOT + '/info/status')
-      .then( async (_res) => {
-        if (_res.ok) {
-          return {
-            ...(await _res.json()),
-            status: 'Online'
-          } as SequencerStatus
-        }
-        else {
-          return {
-            lobby_size: 0,
-            num_contributions: 0,
-            sequencer_address: '0x000',
-            status: 'Offline',
-          } as SequencerStatus
-        }
-      })
-      .then((data) => {
-        resolve(data)
-      })
-    })
+    let result: SequencerStatus
+    try {
+      result = await fetch(API_ROOT + '/info/status')
+      .then((_res) => _res.json())
+      result.status = 'Online'
+    } catch (error) {
+      result = {
+        lobby_size: 0,
+        num_contributions: 0,
+        sequencer_address: '0x000',
+        status: 'Offline',
+      }
+    }
+    return result
   })
 }
