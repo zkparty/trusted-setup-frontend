ed22a7e3a7835baa172d8f666d99cafe25c5c59a
Geoff Lamperd <glamperd@gmail.com>
2022-11-22 13:24:13 +1100

    adds .env for IPFS

--- src/constants.ts
+++ src/constants.ts
@@ -25,7 +25,7 @@ const RADIUS = {
 
 const API_ROOT = process.env.REACT_APP_API_ROOT || 'https://kzg-ceremony-sequencer-dev.fly.dev'
 const SIGNIN_REDIRECT_URL =
-  process.env.REACT_APP_SIGNIN_REDIRECT_URL || 'https://ipfs.io/ipns/k51qzi5uqu5dgiylz6kmdtmvqe94uxwmxbap4v1e3wh91x31wuxnuoil15pel8/#/redirect'
+  process.env.REACT_APP_SIGNIN_REDIRECT_URL || 'https://localhost:3000/redirect'
 const LOBBY_CHECKIN_FREQUENCY =
   parseInt(process.env.REACT_APP_LOBBY_CHECKIN_FREQUENCY as string) || 25000
 const MIN_MOUSE_ENTROPY_SAMPLES =
