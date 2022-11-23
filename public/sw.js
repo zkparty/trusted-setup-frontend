8940b5b95047cf9e36905023d4ca29bc86602b59
Nico Serrano <nnicosp@hotmail.com>
2022-11-22 21:34:48 -0500

    sw(feat): WIP set headers

---
+++ public/sw.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-restricted-globals */
+// sw.js
+self.addEventListener("install", function () {
+    self.skipWaiting();
+  });
+
+  self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+  });
+
+  self.addEventListener("fetch", function (event) {
+    if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+      return;
+    }
+
+    event.preventDefault();
+    event.respondWith(
+      fetch(event.request)
+        .then(function (response) {
+          // It seems like we only need to set the headers for index.html
+          // If you want to be on the safe side, comment this out
+          //if (!response.url.includes("index.html")) return response;
+          //TODO: Refine this to detect only IPFS fetches?
+
+          const coep = 'require-corp';
+          const coop = 'same-origin';
+          if ( event.request.url.includes('.double_sign') ){
+            //coep = 'all'; // TODO: change value
+            console.log('INSIDE IF ====>')
+            coop = 'same-origin-allow-popups'; // TODO: change value
+          }
+
+          const newHeaders = new Headers(response.headers);
+          newHeaders.set("Cross-Origin-Embedder-Policy", coep);
+          newHeaders.set("Cross-Origin-Opener-Policy", coop);
+
+          const moddedResponse = new Response(response.body, {
+            status: response.status,
+            statusText: response.statusText,
+            headers: newHeaders,
+          });
+
+          return moddedResponse;
+        })
+        .catch(function (e) {
+          console.error(e);
+        })
+    );
+  });
