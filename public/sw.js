577adab1ea426def040f8c5b39be7ad4ff45f325
Nico Serrano <nnicosp@hotmail.com>
2022-11-25 16:50:48 -0500

    sw(fix): http headers on Brave

--- public/sw.js
+++ public/sw.js
@@ -3,37 +3,44 @@
 self.addEventListener("install", function () {
     self.skipWaiting();
   });
-  
+
   self.addEventListener("activate", (event) => {
     event.waitUntil(self.clients.claim());
   });
-  
-  self.addEventListener("fetch", function (event) {
+
+  self.addEventListener("fetch", async function (event) {
     if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
       return;
     }
-  
+
+    let origin = self.location.origin;
+    let regex = new RegExp("^" + origin);
+    let url = event.request.url;
+    // discard requests coming outside from the origin (google fonts, apis, etc)
+    if ( !regex.test(url) ) return;
+
+    // Brave requires to use the url string rather than the whole object
+    let request = event.request;
+    if (url.includes('session_id')){
+      request = event.request.url;
+    }
+
     event.respondWith(
-      fetch(event.request)
+      fetch(request)
         .then(function (response) {
-          // It seems like we only need to set the headers for index.html
-          // If you want to be on the safe side, comment this out
-          //if (!response.url.includes("index.html")) return response;
-          //TODO: Refine this to detect only IPFS fetches?
-  
           const newHeaders = new Headers(response.headers);
           newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
           newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
-  
+
           const moddedResponse = new Response(response.body, {
             status: response.status,
             statusText: response.statusText,
             headers: newHeaders,
           });
-  
+
           return moddedResponse;
         })
-        .catch(function (e) {
+        .catch(async function (e) {
           console.error(e);
         })
     );
