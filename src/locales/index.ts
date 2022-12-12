ab74f8d5f935964181d31b08892dfd86c679bd96
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-12-12 14:51:25 -0800

    import translations from Crowdin

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,4 +1,35 @@
+import ar from './ar/translation.json'
+import bn from './bn/translation.json'
+import de from './de/translation.json'
 import en from './en/translation.json'
 import es from './es/translation.json'
+import fr from './fr/translation.json'
+import hi from './hi/translation.json'
+import id from './id/translation.json'
+import it from './it/translation.json'
+import ja from './ja/translation.json'
+import ko from './ko/translation.json'
+import pt from './pt/translation.json'
+import ru from './ru/translation.json'
+import sw from './sw/translation.json'
+import tr from './tr/translation.json'
+import zh from './zh/translation.json'
 
-export const locales = { en, es }
+export const locales = {
+  ar,
+  bn,
+  de,
+  en,
+  es,
+  fr,
+  hi,
+  id,
+  it,
+  ja,
+  ko,
+  pt,
+  ru,
+  sw,
+  tr,
+  zh
+}
