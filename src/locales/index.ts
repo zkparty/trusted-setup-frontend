f6cb9b61199183c7612a24f7118e802acfdd5f17
Nico Serrano <nnicosp@hotmail.com>
2022-12-13 23:24:18 -0500

    languages(feat): added simplified english language

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,6 +3,7 @@ import bn from './bn/translation.json'
 import de from './de/translation.json'
 import en from './en/translation.json'
 import es from './es/translation.json'
+import sim_en from './sim_en/translation.json'
 import fr from './fr/translation.json'
 import hi from './hi/translation.json'
 import id from './id/translation.json'
@@ -15,11 +16,13 @@ import sw from './sw/translation.json'
 import tr from './tr/translation.json'
 import zh from './zh/translation.json'
 
+
 export const locales = {
   ar,
   bn,
   de,
   en,
+  sim_en,
   es,
   fr,
   hi,
