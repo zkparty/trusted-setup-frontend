74c8715746ac0b49f02e7e033545cf254772dddc
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-01-12 19:31:57 -0800

    add useLanguage hook
    reads language noted in query param and sets page i18n context to this language. Enables the ability to pass language preference to new tabs, such as with the ceremony and contributions links

---
+++ src/hooks/useLanguage.ts
@@ -0,0 +1,12 @@
+import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { LANG_QUERY_PARAM, DEFAULT_LANG } from '../constants'
+
+export default function useLanguage() {
+  const { i18n } = useTranslation()
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const lang: string = params.get(LANG_QUERY_PARAM) || DEFAULT_LANG
+    i18n.changeLanguage(lang)
+  }, [i18n])
+}
